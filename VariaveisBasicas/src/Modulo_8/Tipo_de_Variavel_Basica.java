f5c2a0ec0930423393fb91cdc045b12eeb47282a
TABOAODASERRA-AMELIA <TABOAODASERRA-AMELIA@LAPTOP-QPKENP8T>
2023-10-19 17:21:28 -0300

    Tipos de Variáveis

---
+++ VariaveisBasicas/src/Modulo_8/Tipo_de_Variavel_Basica.java
@@ -0,0 +1,38 @@
+package Modulo_8;
+
+public class Tipo_de_Variavel_Basica {
+
+	public static void main(String[] args) {
+		
+		/**
+		 * Tipos de Variáveis
+		 * 
+		 * Variáveis Primitivas: Estas são variáveis que armazenam valores simples, não são objetos. Existem oito tipos primitivos no Java:
+
+			byte: 8 bits, armazena valores inteiros.
+			short: 16 bits, armazena valores inteiros.
+			int: 32 bits, armazena valores inteiros.
+			long: 64 bits, armazena valores inteiros longos.
+			float: 32 bits, armazena valores de ponto flutuante.
+			double: 64 bits, armazena valores de ponto flutuante.
+			char: 16 bits, armazena caracteres Unicode.
+			boolean: armazena valores true ou false.
+			Variáveis de Referência: Estas variáveis referenciam objetos em memória. Os tipos de variáveis de referência incluem:
+			
+			Classes (tipos de classe que você cria)
+			Interfaces (tipos abstratos que definem contratos)
+			Arrays (matrizes de objetos ou primitivos)
+			Variáveis Locais: Variáveis declaradas em um método ou bloco e só existem durante a execução desse método ou bloco.
+			
+			Variáveis de Instância: Variáveis pertencentes a uma instância de uma classe e são inicializadas quando um objeto da classe é criado.
+			
+			Variáveis de Classe (ou estáticas): Variáveis associadas à classe em vez de uma instância específica e compartilhadas por todas as instâncias dessa classe.
+			
+			Variáveis Final: São constantes que não podem ser modificadas após a sua atribuição.
+			
+			Variáveis Voláteis: Usadas em programação concorrente para indicar que uma variável pode ser modificada por várias threads.
+					 
+		 */
+		
+	}
+}
---
+++ VariaveisBasicas/src/br/com/tteles/Exemplos.java
@@ -0,0 +1,42 @@
+package br.com.tteles;
+
+/**
+ * @author tarcio.teles
+ */
+
+public class Exemplos {
+	
+	private int codigo = 2;
+	
+	private long codigoMaior = 123124214;
+	
+	private double valorDecimal1 = 10.3;
+	
+	private float valorDecimal = 10.3f;
+	
+	private String texto;
+	
+	private boolean status;
+	
+	private short shor;
+	
+	private  byte bi;
+	
+	public String retornaTexto() {
+		return "Hello Word";
+	}
+	
+	public String retornaInteiro() {
+		return 150;
+	}
+	
+	public long retornaLong(Long num) {
+		return num;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
